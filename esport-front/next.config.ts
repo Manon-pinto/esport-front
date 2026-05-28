65cca519c006ba2839d82c171da34462b6075fce
Manon-pinto <manoonpnt@gmail.com>
2026-05-28 19:37:26 +0200

    feat: configuration Docker production complète

--- esport-front/next.config.ts
+++ esport-front/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone", // nécessaire pour le Dockerfile de production
 };
 
 export default nextConfig;
